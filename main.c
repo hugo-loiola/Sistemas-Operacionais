2f14090ebb84b9a2292158bcc32be9608b2c416d
Hugo Loiola <hugoloiola.1@gmail.com>
2024-05-14 15:43:27 -0300

    feito a criação

--- main.c
+++ main.c
@@ -12,9 +12,28 @@ typedef struct
 
 } Pokemon;
 
+Pokemon criarPokemon(char Nome[50], int HP, int Atk, int Dfs);
+void mostrarPokemon(Pokemon X);
+
 int main()
 {
   // Criar um pokémon
-  Pokemon Charmander, Bulbasaur, Squirtle;
+  Pokemon X = criarPokemon("Charmander", 100, 12, 32);
+  mostrarPokemon(X);
   return 0;
+}
+
+Pokemon criarPokemon(char Nome[50], int HP, int Atk, int Dfs)
+{
+  Pokemon novoPokemon;
+  strcpy(novoPokemon.Nome, Nome);
+  novoPokemon.HP = HP;
+  novoPokemon.Atk = Atk;
+  novoPokemon.Dfs = Dfs;
+  return novoPokemon;
+}
+
+void mostrarPokemon(Pokemon X)
+{
+  printf("O Pokémon %s possui HP: %d, Atk: %d, Dfs: %d\n", X.Nome, X.HP, X.Atk, X.Dfs);
 }
